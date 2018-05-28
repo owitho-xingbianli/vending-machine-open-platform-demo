4785fb09ccde7eb4d326cf8f366e7bf1bdee9a28
Yangyang Zhong <yangyang.zhong@owitho.com>
2018-05-28 15:11:32 +0800

    test

--- platform-util/src/test/java/com/owitho/open/ApiUtilTest.java
+++ platform-util/src/test/java/com/owitho/open/ApiUtilTest.java
@@ -34,7 +34,7 @@ public class ApiUtilTest
     /**
      * Rigourous Test :-)
      */
-    public static final String APPID = "3bbdeb4ffa90585a4e87a1aa34e5c644";
+    public static final String APPID = "2922b949feae2e8f870414e9442b0611";
 
     public static final String URL = "https://open.owitho.intra.im";
 
@@ -44,12 +44,12 @@ public class ApiUtilTest
     }
 
     public void testGetAccessToken() throws Exception {
-        ResponseModel<TokenInfo> response = OpenApiUtil.getAccessToken(APPID, URL + "/getAccessToken", "bc568e9e-159f-40ce-a9c5-3fba2d3482e8");
+        ResponseModel<TokenInfo> response = OpenApiUtil.getAccessToken(APPID, URL + "/getAccessToken", "a0fdb1a8-5202-4306-b818-67bbbcff1ba0");
         System.out.println(response);
     }
 
     public void testRemoteInvoke() throws Exception {
-        ResponseModel<TokenInfo> response = OpenApiUtil.getAccessToken(APPID, URL + "/getAccessToken", "bc568e9e-159f-40ce-a9c5-3fba2d3482e8");
+        ResponseModel<TokenInfo> response = OpenApiUtil.getAccessToken(APPID, URL + "/getAccessToken", "a0fdb1a8-5202-4306-b818-67bbbcff1ba0");
         ResponseData data = OpenApiUtil.remoteInvokeReturnData(APPID, URL + "/testSign", response.getData().getAccessToken(), new RequestData("XBL001"), ResponseData.class);
         System.out.println(data);
     }
